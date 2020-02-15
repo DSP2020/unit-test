8d8e51c4c5ce5d5b53ee3c8c37fa81f85ff8a35f
erikbustosmo@gmail.com <[erikbustosmo@gmail.com]>
2020-02-15 00:37:40 -0600

    Fixed double tortilla

--- src/main/java/mx/iteso/ut/Quesadilla.java
+++ src/main/java/mx/iteso/ut/Quesadilla.java
@@ -7,26 +7,26 @@ package mx.iteso.ut;
 public class Quesadilla
 {
     private Queso queso;
-    private Tortilla tortilla;
+    private Tortilla tortilla1;
+    private Tortilla tortilla2;
     private int heatLevel;
 
     public String prepareSingle(){
 
-
-     while(getQueso().getCurrentTemperature()< getQueso().getMeltingTemperature() && getTortilla().getCurrentTemperature()< getTortilla().getToastTemperature()){
-         getTortilla().setCurrentTemperature(getTortilla().getCurrentTemperature() + getHeatLevel());
+     while(getQueso().getCurrentTemperature()< getQueso().getMeltingTemperature() && getTortilla1().getCurrentTemperature()< getTortilla1().getToastTemperature()){
+         getTortilla1().setCurrentTemperature(getTortilla1().getCurrentTemperature() + getHeatLevel());
          getQueso().setCurrentTemperature(getQueso().getCurrentTemperature() + getHeatLevel());
-         if (getTortilla().getCurrentTemperature() >= getTortilla().getToastTemperature())
-             getTortilla().toast(true);
+         if (getTortilla1().getCurrentTemperature() >= getTortilla1().getToastTemperature())
+             getTortilla1().toast(true);
          if (getQueso().getCurrentTemperature() >= getQueso().getMeltingTemperature())
              getQueso().melt(true);
      }
 
-     if(getQueso().isMelted() && getTortilla().isToasted())
+     if(getQueso().isMelted() && getTortilla1().isToasted())
          return "Perfect quesadilla";
-     if(getQueso().isMelted() && !getTortilla().isToasted())
+     if(getQueso().isMelted() && !getTortilla1().isToasted())
          return "Good quesadilla";
-     if(!getQueso().isMelted() && getTortilla().isToasted())
+     if(!getQueso().isMelted() && getTortilla1().isToasted())
          return "Terrible quesadilla";
      else
          return "You ran out of gas";
@@ -41,7 +41,26 @@ public class Quesadilla
         // tortilla 1 no tostada, tortilla 2 no tostada, queso derretido
         // tortilla 1 tostada, tortilla 2 tostada, queso no derretido
 
-        return "";
+        while (getQueso().getCurrentTemperature() < getQueso().getMeltingTemperature()
+                && getTortilla1().getCurrentTemperature() < getTortilla1().getToastTemperature()
+                && getTortilla2().getCurrentTemperature() < getTortilla2().getToastTemperature()) {
+
+            if (getTortilla2().getCurrentTemperature() >= getTortilla2().getToastTemperature())
+                getTortilla2().toast(true);
+            if (getTortilla1().getCurrentTemperature() >= getTortilla1().getToastTemperature())
+                getTortilla1().toast(true);
+            if (getQueso().getCurrentTemperature() >= getQueso().getMeltingTemperature())
+                getQueso().melt(true);
+        }
+
+        if (getQueso().isMelted() && getTortilla1().isToasted() && getTortilla2().isToasted())
+            return "Perfect quesadilla";
+        if (getQueso().isMelted() && (!getTortilla1().isToasted() || !getTortilla2().isToasted()))
+            return "Good quesadilla";
+        if (!getQueso().isMelted() && (getTortilla1().isToasted() || getTortilla2().isToasted()))
+            return "Terrible quesadilla";
+        else
+            return "You ran out of gas";
     }
 
     public Queso getQueso() {
@@ -53,12 +72,20 @@ public class Quesadilla
     }
 
 
-    public Tortilla getTortilla() {
-        return tortilla;
+    public Tortilla getTortilla1() {
+        return this.tortilla1;
     }
 
-    public void setTortilla(Tortilla tortilla) {
-        this.tortilla = tortilla;
+    public void setTortilla1(Tortilla tortilla) {
+        this.tortilla1 = tortilla;
+    }
+
+    public Tortilla getTortilla2() {
+        return this.tortilla2;
+    }
+
+    public void setTortilla2(Tortilla tortilla) {
+        this.tortilla2 = tortilla;
     }
 
     public int getHeatLevel() {
